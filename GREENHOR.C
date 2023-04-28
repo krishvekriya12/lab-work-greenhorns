2eb68583d1b13621dcb6a5c1a47623e42e9063ae
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-04-28 15:31:27 +0530

    Add files via upload

---
+++ GREENHOR.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("\n\t*\t*\t*\t\*\t*\t*\t*\n");
+	printf("\n\t\t\t\t\t\t\t\n");
+	printf("\n*\t*\t*\t*\t*\t*\t*\t*\t\n");
+	printf("\n*\t\t*\t\t\t\t\t*\n");
+	printf("\n*\t\t*\t\t*\t*\t*\t*\n");
+	printf("\n*\t\t*\t\t*\t\t*\t*\n");
+	printf("\n*\t*\t*\t*\t*\t*\t*\t*\n");
+	getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
